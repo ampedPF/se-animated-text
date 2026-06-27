15dc6cfe7e929ecdfb7418d57aad9bb1f05a4ec0
ampedPF <ampedpf@gmail.com>
2026-06-28 00:49:26 +0200

    Adds horizontal and vertical setup & new features in test mode

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # SteamElements Animated Text
 
-A StreamElements' custom widget to display animated text.  
+A StreamElements' custom widget to display 2 animated texts in rotation.  
 
 1. Create an overlay from StreamElements' [Dashboard](https://streamelements.com/dashboard/overlays).
 
-2. Add a `STATIC/CUSTOM > Custom widget`.
+1. Add a `STATIC/CUSTOM > Custom widget`.
 
-3. From the Settings panel, click on the `Open Editor` button.
+1. From the Settings panel, click on the `Open Editor` button.
 
-4. Delete the content of the HTML tab.
+1. Delete the content of the HTML tab.
 
-5. Copy/Paste the content of `index.html` into the HTML tab.
+1. Copy/Paste the content of `index.html` into the HTML tab.
 
-6. Repeat the process for the `style.css` file into the CSS tab.
+1. Repeat the process for the `style.css` file into the CSS tab.
 
-7. Repeat the process for the `script.js` file into the JS tab.
+1. Repeat the process for the `script.js` file into the JS tab.
 
-8. Repeat the process for the `fields.json` file into the FIELDS tab.
+1. Repeat the process for the `fields.json` file into the FIELDS tab.
 
-9. Click on the "Done" button at the bottom right.
+1. Click on the "Done" button at the bottom right.
 
-12. Customize the widget using the other panels on the left side.
+1. Customize the widget using the other panels on the left side.
--- script.js
+++ script.js
@@ -1,41 +1,105 @@
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.text1');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-textWrapper = document.querySelector('.text2');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+let fieldData;
+let textWrapper;
 
-anime.timeline({
-        loop: true
-    })
-    .add({
-      targets: '.text1 .letter',
-      translateY: [100,0],
-      translateZ: 0,
-      opacity: [0,1],
-      easing: "easeOutExpo",
-      duration: 1400,
-      delay: (el, i) => 300 + 50 * i
-    }).add({
-      targets: '.text1 .letter',
-      translateY: [0,-100],
-      opacity: [1,0],
-      easing: "easeInExpo",
-      duration: 1200,
-      delay: (el, i) => 100 + 50 * i
-    })
-    .add({
-      targets: '.text2 .letter',
-      translateY: [100,0],
-      translateZ: 0,
-      opacity: [0,1],
-      easing: "easeOutExpo",
-      duration: 1400,
-      delay: (el, i) => 300 + 50 * i
-    }).add({
-      targets: '.text2 .letter',
-      translateY: [0,-100],
-      opacity: [1,0],
-      easing: "easeInExpo",
-      duration: 1200,
-      delay: (el, i) => 100 + 50 * i
-    });
+
+function main() {
+    // Wrap every word in a div and each letter in an individual div
+    textWrapper = document.querySelector('.text1');
+    textWrapper.innerHTML = setupText(textWrapper);
+    
+    textWrapper = document.querySelector('.text2');
+    textWrapper.innerHTML = setupText(textWrapper);
+
+    if (fieldData.testModeEnabled && fieldData.testModeDisableAnimations) return;
+    
+    anime
+        .timeline({
+            loop: true
+        })
+        .add({
+            targets: '.text1 .letter',
+            translateX: [fieldData.text1TranslateXIn, 0],
+            translateY: [fieldData.text1TranslateYIn, 0],
+            translateZ: 0,
+            opacity: [0, 1],
+            easing: fieldData.text1EasingIn,
+            duration: fieldData.text1DurationIn,
+            delay: (el, i) => fieldData.text1DelayInBase + fieldData.text1DelayInStep * i
+        })
+        .add({
+            targets: '.text1 .letter',
+            translateX: [0, fieldData.text1TranslateXOut],
+            translateY: [0, fieldData.text1TranslateYOut],
+            translateZ: 0,
+            opacity: [1, 0],
+            easing: fieldData.text1EasingOut,
+            duration: fieldData.text1DurationOut,
+            delay: (el, i) => fieldData.text1DelayOutBase + fieldData.text1DelayOutStep * i
+        })
+        .add({
+            targets: '.text2 .letter',
+            translateX: [fieldData.text2TranslateXIn, 0],
+            translateY: [fieldData.text2TranslateYIn, 0],
+            translateZ: 0,
+            opacity: [0, 1],
+            easing: fieldData.text2EasingIn,
+            duration: fieldData.text2DurationIn,
+            delay: (el, i) => fieldData.text2DelayInBase + fieldData.text2DelayInStep * i
+        })
+        .add({
+            targets: '.text2 .letter',
+            translateX: [0, fieldData.text2TranslateXOut],
+            translateY: [0, fieldData.text2TranslateYOut],
+            translateZ: 0,
+            opacity: [1, 0],
+            easing: fieldData.text2EasingOut,
+            duration: fieldData.text2DurationOut,
+            delay: (el, i) => fieldData.text2DelayOutBase + fieldData.text2DelayOutStep * i
+        });
+    //debugger;
+}
+
+function setupText(textWrapper) {
+    let textArray = textWrapper.textContent.split(" ");
+    let textHTML = "";
+    for (let index in textArray) {
+        textHTML += "<div class='word'>"
+        textArray[index] = textArray[index].replace("\\s", " ");
+        textHTML += textArray[index].replace(/./g, "<div class='letter'>$&</div>");
+        textHTML += "</div>"
+    }
+    return textHTML
+}
+
+function setupTestMode() {
+    if (fieldData.testModeEnabled == false) {
+        document.getElementsByTagName('html')[0].style.backgroundColor = "transparent";
+        document.getElementsByTagName('body')[0].style.backgroundColor = "transparent";
+        document.getElementsByClassName('text1')[0].style.backgroundColor = "transparent";
+        document.getElementsByClassName('text2')[0].style.backgroundColor = "transparent";
+    } else {
+        if(!fieldData.testModeDisplayBackgroundColor) {
+            document.getElementsByTagName('html')[0].style.backgroundColor = "transparent";
+            document.getElementsByTagName('body')[0].style.backgroundColor = "transparent";
+        }
+        if(!fieldData.testModeDisplayBox1BackgroundColor) {
+            document.getElementsByClassName('text1')[0].style.backgroundColor = "transparent";
+        }
+        if(!fieldData.testModeDisplayBox2BackgroundColor) {
+            document.getElementsByClassName('text2')[0].style.backgroundColor = "transparent";
+        }
+        if (!fieldData.testModeDisplayText1) {
+            document.querySelector('.text1').innerHTML = "";
+        }
+        if (!fieldData.testModeDisplayText2) {
+            document.querySelector('.text2').innerHTML = "";
+        }
+    }
+}
+
+
+window.addEventListener('onWidgetLoad', function (obj) {
+    fieldData = obj.detail.fieldData;
+    setupTestMode();
+  	main();
+});
